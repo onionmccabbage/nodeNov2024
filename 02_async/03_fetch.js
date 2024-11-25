50a7a2480dffadc6819132401f9d3006465ddd82
onionmccabbage <onionmccabbage@live.com>
2024-11-25 14:30:36 +0000

    async-await for remote API fetch

--- 02_async/03_fetch.js
+++ 02_async/03_fetch.js
@@ -1,6 +1,30 @@
-fetch('https://api.github.com/users/onionmccabbage').then( (response)=>{
-    return response.json()
-} ).then( (j)=>{
-    console.log(j)
-} )
+// by default fetch uses 'GET'
+// fetch('https://api.github.com/users/onionmccabbage').then( (response)=>{
+//     return response.json()
+// } ).then( (j)=>{
+//     console.log(j)
+// } )
 
+// handle 'fetch' with async-await
+const getJson = async (url)=>{
+    try {
+        // we may wait for the fetch to return a promise
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error(response.status)
+        }
+        const data = await response.json()
+        return data
+    }
+    catch(err){
+        console.log(err)
+    }
+    finally{
+        // always runs
+    }
+}
+const main = async ()=>{
+    const myUrl = 'https://api.github.com/users'
+    const githubData = await getJson(myUrl)
+    console.log(githubData)
+}
