f8588862f62f9258dd3f16d7d413ceae11ced8f3
onionmccabbage <onionmccabbage@live.com>
2024-11-25 10:31:17 +0000

    simple web server microservice

--- 01_microservices/01_server.js
+++ 01_microservices/01_server.js
@@ -7,13 +7,19 @@ const port     = 3000
 
 // we can make our own server like this
 const server = http.createServer( (req, res)=>{ // request and response objects
+    // explore sending html
+    // explore routing (conditional responses)
     res.statusCode = 200
-    res.setHeader("Content-Type", "text/plain")
+    res.setHeader("Content-Type", "text/html") // or /json /plain /xml etc.
+    res.write('<h3>Welcome to NodeJS</h3>')
+    res.write('<p>using Node microserver</p>')
     res.end('message from microservice') // send anything we like
 } )
 
 // we need to start our microserver
 server.listen(port, hostName, ()=>{
     // we may use back-tick syntax to inject any pure JS statement
-    console.log(`Server running at http://${hostname}:${port}/`)
+    console.log(`Server running at http://${hostName}:${port}/`)
 })
+
+// use a browser to visit 127.0.0.1:3000
