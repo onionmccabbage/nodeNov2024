c5e13c36c03e4379db1e14ca784155e076210583
onionmccabbage <onionmccabbage@live.com>
2024-11-26 14:24:12 +0000

    a simple fetch for remote API data

--- 08_architecture/src/util/getData.js
+++ 08_architecture/src/util/getData.js
@@ -1,7 +1,7 @@
 export const fetchPhotos = async (id='')=>{ // we may specify default values for arguments
     const APIUrl = 'https://jsonplaceholder.typicode.com/photos'
     try{// use the id as an argument in the URL
-        const response = await fetch(`APIUrl/${id``}`) // GET by default
+        const response = await fetch(`${APIUrl}/${id}`) // GET by default
         if (response['status']==200){
             return response.json() // wait for the JSON to be extracted from the response object
         }
