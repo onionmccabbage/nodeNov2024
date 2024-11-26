14c54c4089b5f1d3207d6453a56412bf7cc1b7e9
onionmccabbage <onionmccabbage@live.com>
2024-11-26 10:13:21 +0000

    working with JSON

--- 04_module_automation/src/index.js
+++ 04_module_automation/src/index.js
@@ -1,2 +1,11 @@
+import pino from "pino" // this will look in the node_modules (due to type : module)
+import { greet, goodbye } from "./util.js"
+
 const a = 'auto '
 console.log(`${a} working`)
+
+// logging with pino
+const logger = pino() // info, warn, error ...
+logger.info(greet('Jane')) // we may use the logger to write to a file/db/api etc.
+logger.warn(goodbye('Floella'))
+logger.error('I need coffee')
