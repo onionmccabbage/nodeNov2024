0e2646c74318a9527a162ee6cbaa88dc0afd002d
onionmccabbage <onionmccabbage@live.com>
2024-11-26 14:02:07 +0000

    using generator syntax

---
+++ 07_ES_features/03_generators.js
@@ -0,0 +1,16 @@
+// efficient way to provide large quantities of sequential data
+// e.g. Fibonacci sequence 1, 2, 3, 5, 8, 13, 21, 34, 55...
+function* fib(){ // we MUST use function* not ()=>{}
+    let first=1, second =2 // declare more than one variable
+    for (let i=0; i<10; i++){
+        let sum = first+second
+        yield sum // yield instead of return
+        first = second
+        second = sum
+    }
+}
+// exercise the code
+const g = fib() // we now have an instance of our generator
+console.log( g.next() ) // 3
+console.log( g.next() ) // 5
+console.log( g.next() ) // 8
