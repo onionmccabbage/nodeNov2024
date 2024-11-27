23870b408853c55d4592c1ef672e7ca17387cbde
onionmccabbage <onionmccabbage@live.com>
2024-11-27 16:02:41 +0000

    typescript type guards

---
+++ 14_finally/type_guards.ts
@@ -0,0 +1,16 @@
+// sometimes we need to ensure we have a particular type before trying to use it
+// (remember - there is no TS at runtime)
+
+// a function to return the size of an entity
+const eg = (sn:string | number)=>{
+    // return how many members in the collection 'sn', or the number to a specific size (dp)
+    if (typeof sn === 'number'){
+        // return to 2dp
+        return sn.toFixed(2) // the ts engine offers hints for number-friendly things
+    } else if (typeof sn == 'string'){
+        // return the length of the string
+        return sn.length //ts will code-hint string options here
+    } else {
+        // ummmmmm
+    }
+}
