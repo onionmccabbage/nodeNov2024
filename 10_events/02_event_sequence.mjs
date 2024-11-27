c6e683fdc062f91657669c9232c11178f70e3c66
onionmccabbage <onionmccabbage@live.com>
2024-11-27 10:15:05 +0000

    web

--- 10_events/02_event_sequence.mjs
+++ 10_events/02_event_sequence.mjs
@@ -1,4 +1,8 @@
 import { EventEmitter } from "events";
+// we might have event emitters in other files
+// remember - once other files have been imported their code behaves as if they 
+// were already in this file
+
 const seqEmitter = new EventEmitter()
 
 seqEmitter.on('sequential', ()=>{
@@ -9,6 +13,11 @@ seqEmitter.on('sequential', ()=>{
 })
 seqEmitter.on('sequential', ()=>{
     console.log('third listener')
+})
+
+// we may choose to remove event listeners
+seqEmitter.removeListener('sequential', ()=>{
+    console.log('second listener')
 })
 
 seqEmitter.emit('sequential') // trigger all three listeners, in order
